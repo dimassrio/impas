e2f113d03878a3c278d31ca95232a8fb2fdf4cf8
Dimas Satrio <dimassrio@outlook.com>
2014-07-16 08:21:13 +0700

    2.21

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -4,9 +4,39 @@
 	<div id="dashboard-content" class="min-height">
 		<div class="row section row-white">
 			<div class="large-12 columns">
+				<h3 class="text-center">DEVELOPMENT TEAM</h3>
 				<div class="row">
-					<div class="large-6  large-offset-3 columns text-center">
-						<h3>Development Team</h3>
+					<div class="large-4 large-offset-2 columns">
+						
+						<h5>CONTENT DEVELOPER</h5>
+						<ul>
+							<li>Ima Normalia Kusmayanti</li>
+							<li>Florita Diana Sari</li>
+							<li>Emma Melia</li>
+						</ul>
+						<h5>SOFTWARE DEVELOPER</h5>
+						<ul>
+							<li>Warih Maharani</li>
+							<li>Dimas Satrio Hutomo</li>
+						</ul>
+						
+					</div>
+
+					<div class="large-4 columns">
+					<h5>ILLUSTRATOR</h5>
+						<ul>
+							<li>Rachman Nurwanto</li>
+						</ul>
+						<h5>ACTOR AND DUBBER</h5>
+						<ul>
+							<li>Mefty septiani</li>
+							<li>Hisyam</li>
+							<li>Ima Normalia Kusmayanti</li>
+						</ul>
+						<h4>MULTIMEDIA</h4>
+						<ul>
+							<li>Irvan Nur Aziz</li>
+						</ul>
 					</div>
 				</div>
 				<div class="row">
